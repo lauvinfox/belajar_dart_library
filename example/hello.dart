0e5517120ad89e9e761aced861b2746e88ae6c3b
lauvinfox <miftahilmanakun@gmail.com>
2023-11-06 16:26:39 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,13 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main() {
+  print(sayHello("Mif"));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Category();
+
+  print(customer);
+  print(category);
+  print(product);
+}
